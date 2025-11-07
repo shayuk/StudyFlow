675faf9a1d65c24a8d23ff25bbc3ccc215bcbd80
shayuk <126827551+shayuk@users.noreply.github.com>
2025-11-07 20:00:26 +0200

    Add API handler for dynamic routes

--- server/api/[[...slug]].ts
+++ server/api/[[...slug]].ts
@@ -1,1 +1,6 @@
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+import handler from './index';
 
+export default function all(req: VercelRequest, res: VercelResponse) {
+  return handler(req, res);
+}
